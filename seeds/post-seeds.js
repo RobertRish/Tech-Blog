296e880f8140ef0b30c8e4adde7b3fcb731ef705
Robby Rish <robbyrish13@gmail.com>
2022-03-23 15:21:29 -0400

    utils/auth

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -4,12 +4,12 @@ const postdata = [
   {
     title: 'Spoons',
     text: 'https://techcrunch.com/',
-    user_id: 10
+    user_id: 4
   },
   {
     title: 'Massive',
     text: 'https://techcrunch.com/2022/03/21/nasas-ingenuity-helicopter-will-keep-flying-on-mars-through-at-least-september/',
-    user_id: 8
+    user_id: 1
   },
   {
     title: 'Abra',
@@ -24,12 +24,12 @@ const postdata = [
   {
     title: 'Alakazam',
     text: 'https://techcrunch.com/startups/',
-    user_id: 7
+    user_id: 2
   },
   {
     title: 'Machines',
     text: 'https://techcrunch.com/video/',
-    user_id: 4
+    user_id: 3
   }
 ];
 
